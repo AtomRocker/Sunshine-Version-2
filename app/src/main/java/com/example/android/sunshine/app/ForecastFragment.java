83a45381d2a7241af7c30f14200bfc0ed3ccb935
AtomRocker <AtomRocker@users.noreply.github.com>
2015-11-22 14:32:28 +0200

    Add menu option to display location in map

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -139,7 +139,7 @@ public class ForecastFragment extends Fragment {
                 high = (high * 1.8) + 32;
                 low = (low * 1.8) + 32;
             } else if (!unitType.equals(getString(R.string.pref_units_metric))) {
-                Log.d(LOG_TAG, "Unit typee not found:" + unitType);
+                Log.d(LOG_TAG, "Unit type not found:" + unitType);
             }
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
